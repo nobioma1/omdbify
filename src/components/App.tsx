0c56d36aa1110916c2fc7d99a0d6a10cfb7751bc
Noble Obioma <nobioma1@gmail.com>
2021-01-11 08:27:47 +0100

    add components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,30 @@
 import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 
+import { AppLayout } from './layouts/AppLayout';
+import { Search } from './Search';
+import { Results } from './Results';
+import { Nominations } from './Nominations';
+import { AppContextProvider } from '../contexts/AppContext';
+import { Provider as MovieContextProvider } from '../contexts/MovieContext';
+import { Provider as NominationContextProvider } from '../contexts/NominationContext';
+import { Banner } from './Banner';
+
 export const App = () => {
-  return <ChakraProvider></ChakraProvider>;
+  return (
+    <ChakraProvider>
+      <AppContextProvider>
+        <MovieContextProvider>
+          <NominationContextProvider>
+            <AppLayout>
+              <Search />
+              <Banner />
+              <Results />
+              <Nominations />
+            </AppLayout>
+          </NominationContextProvider>
+        </MovieContextProvider>
+      </AppContextProvider>
+    </ChakraProvider>
+  );
 };
